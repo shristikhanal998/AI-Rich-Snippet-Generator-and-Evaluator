8ff4660f83ee1f45717235319015adabdcfbbaa8
khanalshristi33 <khanalshrishti@gmail.com>
2026-08-29 15:24:07 +0530

    t

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+import joblib
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
